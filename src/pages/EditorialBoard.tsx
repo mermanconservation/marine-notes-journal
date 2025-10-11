6dfb93289ace84b09bee6868f0768569e9ca4506
mermanconservation <mermanconservation@gmail.com>
2025-10-11 10:21:38 +0300

    Update EditorialBoard.tsx

--- src/pages/EditorialBoard.tsx
+++ src/pages/EditorialBoard.tsx
@@ -21,6 +21,14 @@ const EditorialBoard = () => {
       expertise: ["Marine Ecology", "Conservation Biology", "Shark Conservation"],
       email: "info@mermanconservation.co.uk"
     }
+
+        {
+      name: "Charlotte Hayes",
+      affiliation: "",
+      location: "Bristol, UK",
+      expertise: ["Marine Ecology"],
+      email: ""
+    }
   ];
 
   return (
